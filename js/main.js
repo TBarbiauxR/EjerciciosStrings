6508119b7e416d420d7db3a0a7514f6a7adb9b6a
Tannya <tagbr92@hotmail.com>
2022-12-12 13:53:48 -0600

    contar letras

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function contarletras(texto = "") {
+    if (typeof texto !== "string")
+        return "el valor ingresado no es una cadena";
+    if (texto === "") return "no ha ingresado ningun valor"
+return texto.length;
+}
+
+console.log (contarletras)
+
+let contarletras=" parangacutirimicuaro"
+contarletras = mombre.toUppercase ();
+
+for (let index = 0; index < contarletras.length; index++)
+console.log (contarletras.charAT (index));
+let contTemp=1;
+for (let cont = index+1; cont <contarletras.length; cont++ ){
+    if(nombre.charAT(index)== nombre.charAT (cont))
+    contTemp++;
+}    
+
+
